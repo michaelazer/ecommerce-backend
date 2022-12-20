bbb06ab8e87054af6f4436a74cc9894fd30f0b65
Michael Azer <michael.azer@gmail.com>
2022-12-20 21:29:26 +0000

    🔧 fixed add products to order test

--- src/models/tests/order_spec.ts
+++ src/models/tests/order_spec.ts
@@ -32,6 +32,11 @@ describe("Order Model", () => {
     });
   });
 
+  it('create method should add products to order', async () => {
+    const result: Order = await store.addProduct(1, "1", "1")
+    expect(result.id).toEqual(1);
+  })
+
   it('index method should return a list of orders', async () => {
     const result: Order[] = await store.index();
     expect(result.length).toEqual(1);
